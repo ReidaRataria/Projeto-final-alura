1c11f1b0a71a05196c17eceea94ab3e068e7e259
ReidaRataria <lgcp.2006@gmail.com>
2024-12-06 22:46:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Química',
+    'O que é um atomo?',
+    'É aquilo que constitui a matéria, tudo que possui massa e volume'
+)
+
+criaCartao(
+    'História',
+    'Oque foi a tripce aliança?',
+    'foi um acordo militar entre a Alemanha, Áustria-Hungria e Itália'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz cançado em Inglês?',
+    'cançado em ingles é TIRED (TAIRED)'
+)
